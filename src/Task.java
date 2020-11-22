84ae9b25ef323a2362a097f1de39c21b3a0bb326
lolzhe <44326436+lolzhe@users.noreply.github.com>
2020-11-23 04:09:38 +0500

    Добавлено поле имени

--- src/Task.java
+++ src/Task.java
@@ -3,9 +3,11 @@ import jade.core.AID;
 public class Task {
     public AID aid;
     public int complexity = 0;
+    public String name;
 
-    public Task(AID id, int comp) {
+    public Task(AID id, int comp, String _name) {
         aid = id;
         complexity = comp;
+        name = _name;
     }
 }
